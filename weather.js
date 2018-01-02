b3386e2e5d006af11cf1f7ab7aeb248913059815
Andrew Ziegler <14apdjz@gmail.com>
2018-01-01 21:52:09 -0500

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,49 @@
+let data;
+let min;
+let max;
+let current;
+let condition;
+let windSpeed;
+let change;
+// let icon;
+
+$(document).ready(function () {
+   $("#zip").change(function(){
+    change = $(this).val()
+});
+    if(change === undefined){
+        change = 91387;
+    }
+});
+
+function zip(){
+    $.ajax({
+		dataType: 'json',
+		url: 'http://api.openweathermap.org/data/2.5/weather?zip='+change+',us&APPID=yourown',
+		data: data,
+		success: (data) => {
+		  condition = data.weather[0].description;
+			current = data.main.temp *  9 / 5 - 459.67;
+			max = data.main.temp_max *  9 / 5 - 459.67;
+			min = data.main.temp_max *  9 / 5 - 459.67;
+			windSpeed = data.wind.speed * 2.236936;
+
+			document.getElementById('currentWeather').innerHTML = current.toFixed(2);
+			document.getElementById('maxWeather').innerHTML = max.toFixed(2);
+			document.getElementById('minWeather').innerHTML = min.toFixed(2);
+			document.getElementById('windSpeed').innerHTML = windSpeed.toFixed(2);
+			document.getElementById('condition').innerHTML = condition;
+		}
+	});
+}
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+// function showPosition(position) {
+//     x.innerHTML = "Latitude: " + position.coords.latitude + 
+//     "<br>Longitude: " + position.coords.longitude; 
+// }
